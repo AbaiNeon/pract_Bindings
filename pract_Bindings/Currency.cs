d9bb58471a127d85ba78c5237e1ff450e37e48ea
Abai <abai2010@bk.ru>
2018-01-23 19:58:57 +0600

    finished

---
+++ pract_Bindings/Currency.cs
@@ -0,0 +1,128 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract_Bindings
+{
+    public class Сurrency : INotifyPropertyChanged
+    {
+        private int id;
+        private string nameKaz;
+        private string unit;
+        private string ratio;
+        private string nameRus;
+        private string exchangeRates;
+        private string code;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        [JsonProperty("name_kaz")]
+        public string NameKaz
+        {
+            get
+            {
+                return nameKaz;
+            }
+            set
+            {
+                nameKaz = value;
+                OnPropertyChanged("NameKaz");
+            }
+        }
+
+        [JsonProperty("edinica_izmerenia")]
+        public string Unit
+        {
+            get
+            {
+                return unit;
+            }
+            set
+            {
+                unit = value;
+                OnPropertyChanged("Unit");
+            }
+        }
+
+        [JsonProperty("sootnowenie")]
+        public string Ratio
+        {
+            get
+            {
+                return ratio;
+            }
+            set
+            {
+                ratio = value;
+                OnPropertyChanged("Ratio");
+            }
+        }
+
+        [JsonProperty("name_rus")]
+        public string NameRus
+        {
+            get
+            {
+                return nameRus;
+            }
+            set
+            {
+                nameRus = value;
+                OnPropertyChanged("NameRus");
+            }
+        }
+
+        [JsonProperty("kurs")]
+        public string ExchangeRates
+        {
+            get
+            {
+                return exchangeRates;
+            }
+            set
+            {
+                exchangeRates = value;
+                OnPropertyChanged("ExchangeRates");
+            }
+        }
+
+        [JsonProperty("kod")]
+        public string Code
+        {
+            get
+            {
+                return code;
+            }
+            set
+            {
+                code = value;
+                OnPropertyChanged("Code");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+    }
+}
---
+++ pract_Bindings/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pract_Bindings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Сurrency> list1;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            list1 = new ObservableCollection<Сurrency>();
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://data.egov.kz/api/v2/valutalar_bagamdary4/v239?pretty");
+
+            Stream stream = request.GetResponse().GetResponseStream();
+            string html = null;
+            using (stream)
+            {
+                using (StreamReader reader = new StreamReader(stream, Encoding.GetEncoding("utf-8")))
+                {
+                    html = reader.ReadToEnd();
+                }
+            }
+
+            list1 = JsonConvert.DeserializeObject<ObservableCollection<Сurrency>>(html);
+
+            listView.ItemsSource = list1;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            list1.RemoveAt(0);
+        }
+    }
+}
